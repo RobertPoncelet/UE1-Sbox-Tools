92847c099a8cfea5b860a96b6cf1ed64a332bffd
RobertPoncelet <robert.poncelet@talk21.com>
2021-04-16 22:06:25 +0100

    Convert all meshes to FBX using the nice Blender script

---
+++ blender_psk_to_fbx.py
@@ -0,0 +1,35 @@
+import bpy
+import glob
+import os
+import io_import_scene_unreal_psa_psk_280 as psk
+
+ROOT_PATH = "F:/Google Drive/hp_resources"
+#GAMES = ["hp1", "hp2"]
+GAMES = ["hp2"]
+
+for game in GAMES:
+    glob_path = os.path.join(ROOT_PATH, game, "raw_models_textures", "**", "*.psk")
+    psks = glob.glob(glob_path, recursive=True)
+#    psks = psks[:10] # remove for all psks
+    for psk_path in psks:
+        print("Converting " + psk_path)
+        psk.pskimport(psk_path, context=bpy.context, bDontInvertRoot=False)
+        
+        model_name = os.path.splitext(os.path.basename(psk_path))[0]
+        bpy.ops.object.select_all(action="SELECT")
+        bpy.ops.object.shade_smooth()
+#        bpy.context.space_data.context = 'DATA'
+        bpy.data.objects[model_name + ".mo"].data.use_auto_smooth=True
+        bpy.data.objects[model_name + ".mo"].data.auto_smooth_angle = 1.40499
+        
+        #if model_name[:2] == "sk":
+        #    model_name = model_name[2:]
+        #if model_name[-4:] == "Mesh":
+        #    model_name = model_name[:-4]
+        model_name += ".fbx"
+        # This bit's a bit hacky
+        subfolder = psk_path.split(os.path.sep)[-2]
+        out_path = os.path.join(ROOT_PATH, game, "fbx", subfolder, model_name)
+        bpy.ops.export_scene.fbx("EXEC_DEFAULT", filepath=out_path)
+        
+        bpy.ops.object.delete(use_global=True, confirm=False)
