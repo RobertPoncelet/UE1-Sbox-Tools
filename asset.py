790dc24ffaf8bd1cce6bc78b87795d831e69daf0
RobertPoncelet <robert.poncelet@talk21.com>
2022-04-08 14:53:25 +0100

    Properly implement AssetDescription.from_path()

--- asset.py
+++ asset.py
@@ -11,7 +11,7 @@ STAGES = ["original", "intermediate", "converted"]
 GAMES = ["hp1", "hp2"]
 
 ROOT_DICT = {
-    "original": "", # TODO: this is hacky, change to an "original_assets" folder when we get the new style tools working
+    "original": "", # HACK: change to an "original_assets" folder when we get the new style tools working
     "intermediate": "intermediate_assets",
     "converted": "tp_assets"
 }
@@ -31,6 +31,14 @@ CATEGORY_DICT = {
     }
 }
 
+# Find value given key
+# Kinda hacky but maybe we can find a way to change it later
+def reverse_dict(d, value):
+    for k, v in d.items():
+        if v == value:
+            return k
+    raise KeyError("No value {} in dict".format(value))
+
 @dataclass
 class AssetDescription:
     stage: str # Original, intermediate, or converted
@@ -42,7 +50,28 @@ class AssetDescription:
 
     @staticmethod
     def from_path(path):
-        pass # TODO
+        path = os.path.realpath(path)
+        assert(path.lower().startswith(REPO_DIR.lower()))
+        path = path[len(REPO_DIR):]
+        parts = path.split(os.path.sep)
+        if not parts[0]:
+            parts = parts[1:]
+
+        # HACK: there's no original_assets folder yet
+        orig = parts[0] in GAMES
+        if orig:
+            parts.insert(0, "temp")
+
+        assert(len(parts) >= 5)
+
+        _stage = "original" if orig else reverse_dict(ROOT_DICT, parts[0])
+        _game = parts[1]
+        assert(_game in GAMES)
+        _category = reverse_dict(CATEGORY_DICT[_stage], parts[2])
+        _subfolder = os.path.sep.join(parts[3:-1])
+        _name, _filetype = os.path.splitext(parts[-1])
+        _filetype = _filetype[1:]
+        return AssetDescription(_stage, _game, _category, _subfolder, _name, _filetype)
 
     def path(self):
         assert(self.stage in STAGES)
@@ -51,7 +80,8 @@ class AssetDescription:
         root = ROOT_DICT[self.stage]
         category = CATEGORY_DICT[self.stage][self.category]
         # TODO: Why doesn't os.path.join work here???
-        ret = os.path.sep.join([REPO_DIR, 
+        ret = os.path.sep.join([
+            REPO_DIR,
             root, 
             self.game, 
             category, 
